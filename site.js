90cd784a7a0629b52a4846bc84fd20e2d5f9feca
xuyangwu <wuxvyang112358@gmail.com>
2026-08-04 04:02:29 +0800

    Delete site.js

--- site.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const current = window.location.pathname.split("/").pop() || "index.html";
-  document.querySelectorAll(".nav a").forEach(link => {
-    if (link.getAttribute("href") === current) {
-      link.classList.add("active");
-    }
-  });
-});
